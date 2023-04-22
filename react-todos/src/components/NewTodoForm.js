f522fe012147c2ea8b65b93a76df1bcfe79585e3
Rajiv Ranjan <rajivranjan@ZIVAMEs-MacBook-Pro-2.local>
2023-04-22 12:57:02 +0530

    Creating newTodo Form onchange event added

--- react-todos/src/components/NewTodoForm.js
+++ react-todos/src/components/NewTodoForm.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function NewTodoForm(){
+
+    const [description, setDescription]=useState("");
+    const [assigned, serAssigned]=useState("");
+
+
+    const descriptionChange= (event) =>{
+        console.log('description',event.target.value)
+        setDescription(event.target.value);
+    }
+
+    const assignedChange= (event) =>{
+        console.log('assigned',event.target.value);
+        serAssigned(event.target.value);
+    }
 
     return(
 
@@ -8,11 +22,11 @@ function NewTodoForm(){
             <form>
                 <div className='mb-3'>
                   <label className='form-label'>Assigned</label>
-                  <input type="text" className='form-control' required></input>
+                  <input type="text" className='form-control' required onChange={assignedChange} value={assigned}></input>
                 </div>
                 <div className='mb-3'>
                     <label className='form-lable'>Description</label>
-                    <textarea className='form-control' rows={3} required></textarea>
+                    <textarea className='form-control' rows={3} required onChange={descriptionChange} value={description}></textarea>
                 </div>
                 <button type='button' className='btn btn-primary mt-3'>Add Todo</button>
             </form>
