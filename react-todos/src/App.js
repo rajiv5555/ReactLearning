38eec6b3ef5d5949b54532cc024c3d622966da64
Rajiv Ranjan <rajivranjan@ZIVAMEs-MacBook-Pro-2.local>
2023-04-22 12:25:39 +0530

    usestate hook for mantaining state of const or properties

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,13 +1,27 @@
+import React,{useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
 function App() {
 
-  const todos=[
+  const [todos, setTodos] = useState([
     {rowNumber:1, rowDescription:'Feed puppy', rowAssigned:'user one'},
     {rowNumber: 2, rowDescription:'water plants', rowAssigned:'user two'},
     {rowNumber:3, rowDescription:'Make dinner', rowAssigned:'user three'},
     {rowNumber:3, rowDescription:'charge phone battery', rowAssigned:'user four'}
-  ]
+  ])
+
+  const addTodo=()=>{
+    if(todos.length>0){
+      const newTodo={
+        rowNumber: todos.length+1,
+        rowDescription:'New todo',
+        rowAssigned:'User Three'
+      };
+      //todos.push(newTodo);
+      setTodos(todos => [...todos,newTodo])
+      console.log(todos)
+        }
+  }
 
   return (
       <div className='mt-5 container'>
@@ -17,6 +31,7 @@ function App() {
          </div>
          <div className="card-body">
          <TodoTable todos={todos}/>
+         <button className='btn btn-primary' onClick={addTodo}>Add new todo</button>
          </div>
         </div>
       </div>
