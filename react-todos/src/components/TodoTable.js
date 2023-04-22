33bbefa79e41298ff5d5f705b63d4a9ec8feb9c1
Rajiv Ranjan <rajivranjan@ZIVAMEs-MacBook-Pro-2.local>
2023-04-22 10:42:49 +0530

    Basic components rendering, and pass data from component and render through props

---
+++ react-todos/src/components/TodoTable.js
@@ -0,0 +1,19 @@
+function TOdoTable(props){
+
+    return(
+        <table table table-hover>
+            <thead>
+            <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TodoRowItem rowNumber={todos[0].rowNumber} rowDescription={todos[0].rowDescription} rowAssigned={todos[0].rowAssigned}/>
+              <TodoRowItem rowNumber={todos[1].rowNumber} rowDescription={todos[1].rowDescription} rowAssigned={todos[1].rowAssigned}/>
+              <TodoRowItem rowNumber={todos[2].rowNumber} rowDescription={todos[2].rowDescription} rowAssigned={todos[2].rowAssigned}/>
+            </tbody>
+        </table>
+    )
+}
